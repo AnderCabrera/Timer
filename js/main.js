b5c150ad2dad07252ca28125ad22eabf6c62e3d6
Ander Cabrera <andercabrerac@gmail.com>
2022-04-24 21:09:06 -0600

    lateral menu img positioned

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
   const number = document.getElementById('main__h2')
   const addBtn = document.getElementById('main__button-1');
   const substractBtn = document.getElementById('main__button-2');
   const startBtn = document.getElementById('main__button-3');
   const stopBtn = document.getElementById('main__button-4');
+  const resetBtn = document.getElementById('main__button-5');
 
   let pause = false;
   let count = 0;
-
 
   number.innerText = `${count} seg`;
 
   addBtn.onclick = () => {
     count += 5;
     number.innerText = `${count} seg`;
+    if (count === 0) {
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
   }
 
   substractBtn.onclick = () => {
@@ -33,14 +37,16 @@ document.addEventListener('DOMContentLoaded', function () {
     startBtn.innerText = 'Start';
     startBtn.style.display = 'none';
     stopBtn.style.display = 'block';
+    pause = !pause
     counter = setInterval(() => {
       if (count === 0) {
-        addBtn.disabled = false;
         substractBtn.disabled = false;
+        addBtn.disabled = false;
         startBtn.disabled = false;
         stopBtn.disabled = true;
         startBtn.innerText = 'Start';
         startBtn.style.display = 'block';
+        stopBtn.style.display = 'none';
         clearInterval(counter);
       } else {
         count--;
@@ -49,10 +55,12 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 1000);
   }
 
+  stopBtn.style.display = 'none';
   stopBtn.disabled = true;
   stopBtn.onclick = () => {
+    //pause is false
     pause = !pause;
-    if (pause) {
+    if (!pause) { //pause is true by the !pause
       clearInterval(counter);
       stopBtn.innerText = 'Pause';
       startBtn.disabled = false;
@@ -62,8 +70,21 @@ document.addEventListener('DOMContentLoaded', function () {
       startBtn.innerText = 'Reanudar';
       startBtn.style.display = 'block';
       stopBtn.style.display = 'none';
+      resetBtn.disabled = false;
     }
   }
 
-
-})
+  resetBtn.disabled = true;
+  resetBtn.onclick = () => {
+    count = 0;
+    number.innerText = `${count} seg`;
+    addBtn.disabled = false;
+    substractBtn.disabled = false;
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    resetBtn.disabled = true;
+    startBtn.innerText = 'Start';
+    startBtn.style.display = 'block';
+    stopBtn.style.display = 'none';
+    clearInterval(counter);
+  }
